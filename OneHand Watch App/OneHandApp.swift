d4d0d55ff7c51cfab356de279d4a9666c00e1fe2
Fidel Coria <fidel.coria@aa.com>
2024-06-14 21:04:24 -0500

    Initial Commit

---
+++ OneHand Watch App/OneHandApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneHandApp.swift
+//  OneHand Watch App
+//
+//  Created by Fidel Coria on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct OneHand_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
