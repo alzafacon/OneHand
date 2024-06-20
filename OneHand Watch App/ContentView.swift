27cf92e0cfbf7f806f1f2e58e0f48b0a44c607e6
Fidel Coria <fidel.coria@aa.com>
2024-06-19 23:32:10 -0500

    rotate hand based on time

--- OneHand Watch App/ContentView.swift
+++ OneHand Watch App/ContentView.swift
@@ -11,7 +11,10 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             Image("clockFace")
+                .scaleEffect(CGSize(width: 1.5, height: 1.5))
             Image("hourHand")
+                .scaleEffect(CGSize(width: 1.5, height: 1.5))
+                .rotationEffect(.degrees(calcTimeDegrees(hour: 1, minute: 45)))
         }
         .padding()
     }
@@ -22,6 +25,17 @@ func getTime() -> String {
     formatter.timeStyle = .short
     let dateString = formatter.string(from: Date())
     return dateString
+}
+
+func calcTimeDegrees(hour: Double, minute: Double) -> Double {
+    // let pole lay on positive y-axis to start angle calculation
+    var time = 0.0
+    // advance hour
+    time += hour * 30 // 30 degrees in 1 hour
+    // advance minute
+    time += minute * 0.5 // 0.5 degress in 1 minute
+
+    return time
 }
 
 #Preview {
