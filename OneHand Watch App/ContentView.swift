3edeed1db561121225bb9631f5983de6ad8908e7
Fidel Coria <fidel.coria@aa.com>
2024-06-20 21:48:16 -0500

    fix the math

--- OneHand Watch App/ContentView.swift
+++ OneHand Watch App/ContentView.swift
@@ -10,29 +10,31 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        // TODO verify assumption: svg fills viewSize size area
-        
-        let hour = 12.0
-        let minute = 0.0
+        let hour = 7.0
+        let minute = 45.0
         let timeAngle = calcTimeDegrees(hour: hour, minute: minute)
         
         // calculations are relative to viewSize
-        let viewSize = 200.0
+        let viewSize = 118.0 // value from svg sizes
         let radius = viewSize / 2
-        let zoom = 0; // [0,1]. 1 means the diameter of the viewport is congruent with a tangent to the clock face
-        let chordOffset = getChordOffset(zoom: radius, radius: radius)
+        let zoom = 0.95; // [0,1). 1 means the diameter of the viewport is congruent with a tangent to the clock face
+        let chordOffset = getChordOffset(zoom: zoom, radius: radius)
         let scaleFactor = getScaleFactor(chordOffset: chordOffset, radius: radius)
         
-        let dx = getDeltaX(chordOffset: scaleFactor * chordOffset, timeDegrees: timeAngle)
-        let dy = getDeltaY(chordOffset: scaleFactor * chordOffset, timeDegrees: timeAngle)
-        
+        let dx = scaleFactor * getDeltaX(chordOffset: chordOffset, timeDegrees: timeAngle)
+        let dy = scaleFactor * getDeltaY(chordOffset: chordOffset, timeDegrees: timeAngle)
+        // yes, the x and y are flipped and there is a negative sign intentionally
+        let translation = CGAffineTransform(translationX: -dy, y: dx)
+
         ZStack {
             Image("clockFace")
-                .frame(width: viewSize, height: viewSize, alignment: .center)
                 .scaleEffect(CGSize(width: scaleFactor, height: scaleFactor))
+                .transformEffect(translation)
             Image("hourHand")
-                .scaleEffect(CGSize(width: scaleFactor, height: scaleFactor))
                 .rotationEffect(.degrees(timeAngle))
+                .scaleEffect(CGSize(width: scaleFactor, height: scaleFactor))
+                .transformEffect(translation)
+                
         }
         .padding()
     }
